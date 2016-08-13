7b1c0523f2cf28e435bc4057d4a1ea0bf99eca69
Eduar Apaza Yáñez <educartoons@gmail.com>
2016-08-12 21:31:48 -0500

    Initial Commit

---
+++ Pointers/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Pointers
+//
+//  Created by Eduar Apaza Yáñez on 8/12/16.
+//  Copyright © 2016 Eduar Apaza Yáñez. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
