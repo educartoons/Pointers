1e3d07b150276bf85bf1e5449f7a294f0c01790b
Eduar Apaza Yáñez <educartoons@gmail.com>
2016-08-12 22:20:52 -0500

    Working with pointers first part.

--- Pointers/main.cpp
+++ Pointers/main.cpp
@@ -5,11 +5,47 @@
 //  Created by Eduar Apaza Yáñez on 8/12/16.
 //  Copyright © 2016 Eduar Apaza Yáñez. All rights reserved.
 //
+//  Using subindexes and pointers with arrays
+//
 
 #include <iostream>
 
+using std::cout;
+using std::endl;
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    
+    int b[] = {10, 20, 30, 40};
+    
+    int *pointer = b;
+    
+    //showing the elements from b array using the subindex
+    
+    for(int i=0; i<4; i++){
+        cout<<b[i]<<" - ";
+    }
+    cout<<endl;
+    
+    //showing the elements from b array using *pointer like subindex
+    
+    for(int j=0; j<4; j++){
+        cout<<pointer[j]<<" - ";
+    }
+    
+    cout<<endl;
+    
+    //showing the elements from b array using *pointer and the notation pointer
+    
+    for(int k=0; k<4; k++){
+        cout<<*(pointer+k)<<" - ";
+    }
+    
+    cout<<endl;
+    
+    cout<<" ------ ";
+    
+    //Copying characters from a string with arrays and pointers
+    
+    
+    
 }
